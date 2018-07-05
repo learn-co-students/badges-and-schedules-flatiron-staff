5aab81d4c11ef01d2f30d709764eb1009d6ca9ac
Emily Jennings <emily.jennings@flatironschool.com>
2018-07-05 18:15:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+      badges << badge_maker("#{name}")
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  index = 0
+  attendees.each_with_index do |name|
+    room_assignments << {badges[index], "#{name}"
+    index += 1
+  end
+end
